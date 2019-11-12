fa2088434196e59667f5a7cf476cdc24539543bf
anasgit95 <anas_zay@outlook.fr>
2019-11-12 15:38:25 +0100

    Adding style

--- src/Formulaire/Formulaire.js
+++ src/Formulaire/Formulaire.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faUser, faVoicemail, faCreditCard, faAddressCard, faUnlock, faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
 import './Formulaire.css' ;
+import logo from './experts.png'
 class  formulaire extends React.Component {
     constructor(props) {
         super(props);
@@ -11,7 +14,7 @@ class  formulaire extends React.Component {
                      
                       reponse:'',
                       error:false,
-                      placeNom:"ecrivez votre nom Monsieur",
+                      placeNom:"password",
                       NameColor: 'grey'
                       };
     
@@ -56,25 +59,33 @@ class  formulaire extends React.Component {
         return ( 
         
           <form onSubmit={this.handleSubmit} className="Essayer"> 
-      { reponse===200?   <div> BienVenu {this.state.formFields.username } Chez EXPERT SOLUTIONS
+      { reponse===200?   <div className="Accueil"> BienVenu {this.state.formFields.username } Chez EXPERT SOLUTIONS
       </div>           
-            :<div> 
-           <p> BIENVENU CHEZ EXPERT SOLUTIONS</p>
+            :<div className="Accueil" > 
+           <img src={logo} className="ImageExp" alt="Logo" />
+           <br/>
+
             <label>
-               
-              <input className={classes} type="text" name="username" value={this.state.formFields.username} onChange={this.handleChange} placeholder="Ecrivez votre nom"  style={{borderColor :this.state.NameColor}}/>
-            
+
+              <input className={classes} type="text" name="username" value={this.state.formFields.username} onChange={this.handleChange} placeholder="Username"  style={{borderColor :this.state.NameColor}}/>
+              <FontAwesomeIcon icon={faUser} />
+
             </label>
             <br/>
             <label>
             
               <input type="password" value={this.state.formFields.password} name="password" onChange={this.handleChange} placeholder={this.state.placeNom} style={{borderColor :this.state.NameColor}} />
+              <FontAwesomeIcon icon={faUnlock} />
+
             </label>
             <br/>
             <label>
            
                
-              <input type="email" value={this.state.formFields.email} name="email"  onChange={this.handleChange} placeholder="Ecrivez votre Email" style={{borderColor :this.state.NameColor}}   />
+              <input type="email" value={this.state.formFields.email} name="email"  onChange={this.handleChange} placeholder="User@User.fr" style={{borderColor :this.state.NameColor}}  />
+              <FontAwesomeIcon icon={faEnvelopeOpen} />
+              
+
             </label>
             <br/>
             <a href="/registre" className="Registre">Registre </a>
--- src/Registre/Registre.js
+++ src/Registre/Registre.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../Formulaire/Formulaire.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import {  faUser,  faEnvelopeOpen, faEye } from '@fortawesome/free-solid-svg-icons'
+
 class  registre extends React.Component {
     constructor(props) {
         super(props);
@@ -9,17 +13,18 @@ class  registre extends React.Component {
                       password: '',
                       email:'',
                       lastname:''},
-                     
+                      type:'password',
                       reponse:'',
                       error:false,
-                      placeNom:"ecrivez votre nom Monsieur",
+                      placeNom:"Username",
                       NameColor: 'grey'
                       };
     
         this.handleChange = this.handleChange.bind(this);
-
+            
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.ShowPassword=this.ShowPassword.bind(this);
       }
     
       handleChange(event) {
@@ -31,7 +36,13 @@ class  registre extends React.Component {
 
 
 }
-     
+ShowPassword(event ) {
+  if(this.state.type==="password") { 
+this.setState({type:"text"});
+}
+else this.setState({type:"password"});
+
+}
     
       handleSubmit(event) {
         if((this.state.formFields.username )&&(this.state.formFields.email) && (this.state.formFields.password) ) { 
@@ -57,30 +68,42 @@ class  registre extends React.Component {
         return ( 
         
           <form onSubmit={this.handleSubmit} className="Essayer"> 
-      { reponse===200?   <div> BienVenu {this.state.formFields.username } Chez EXPERT SOLUTIONS</div>           
-            :<div> 
+      { reponse===200?   <div className="Accueil"> BienVenu {this.state.formFields.username } Chez EXPERT SOLUTIONS</div>           
+            :<div className="Accueil"> 
            <p> BIENVENU CHEZ EXPERT SOLUTIONS</p>
             <label>
                
-              <input className={classes} type="text" name="username" value={this.state.formFields.username} onChange={this.handleChange} placeholder="Ecrivez votre nom"  style={{borderColor :this.state.NameColor}}/>
-            
+              <input className={classes} type="text" name="username" value={this.state.formFields.username} onChange={this.handleChange} placeholder="Nom"  style={{borderColor :this.state.NameColor}}/>
+              <FontAwesomeIcon icon={faUser} />
+
             </label>
             <br/>
             <label>
                
-               <input className={classes} type="text" name="lastname" value={this.state.formFields.lastname} onChange={this.handleChange} placeholder="Ecrivez votre nom"  style={{borderColor :this.state.NameColor}}/>
-             
+               <input className={classes} type="text" name="lastname" value={this.state.formFields.lastname} onChange={this.handleChange} placeholder="Prenom"  style={{borderColor :this.state.NameColor}}/>
+               <FontAwesomeIcon icon={faUser}  />
+
+               <br/>
+
              </label>
             <label>
             
-              <input type="password" value={this.state.formFields.password} name="password" onChange={this.handleChange} placeholder={this.state.placeNom} style={{borderColor :this.state.NameColor}} />
+              <input type={this.state.type} value={this.state.formFields.password} name="password" onChange={this.handleChange} placeholder="password  " style={{borderColor :this.state.NameColor}} />
+              
+               <FontAwesomeIcon icon={faEye} onClick={this.ShowPassword}  className="Cursor"> </FontAwesomeIcon>
+
             </label>
             <br/>
             <label>
            
                
-              <input type="email" value={this.state.formFields.email} name="email"  onChange={this.handleChange} placeholder="Ecrivez votre Email" style={{borderColor :this.state.NameColor}}   />
+              <input type="email" value={this.state.formFields.email} name="email"  onChange={this.handleChange} placeholder="Email" style={{borderColor :this.state.NameColor}}   />
+              <FontAwesomeIcon icon={faEnvelopeOpen} />
+           
             </label>
+            <br/>
+            <a href="/" className="Registre">Login </a>
+
             <br/>
             <button type="submit" value="Envoyer" > Registre </button> 
   
